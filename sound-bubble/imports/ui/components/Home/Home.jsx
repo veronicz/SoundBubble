3092d5fe332c7a4bab839e2f9e652f367288e7f9
Martin Palanca <36322050+martipal@users.noreply.github.com>
2019-06-07 20:02:17 -0700

    Added my components
    meteor not runninng yet

--- sound-bubble/imports/ui/components/Home/Home.jsx
+++ sound-bubble/imports/ui/components/Home/Home.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 
 export default class Home extends Component {
   render() {
-    return <h3>Home</h3>;
+    return (<div>
+      <SongLog/>
+    </div>);
   }
 }
