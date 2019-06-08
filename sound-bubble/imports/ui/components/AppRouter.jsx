b34f36a3b30b02a0ae474340ef97c3fc06da265e
Talos <TalosOops@gmail.com>
2019-06-08 00:32:15 -0700

    Heading and nav component added

--- sound-bubble/imports/ui/components/AppRouter.jsx
+++ sound-bubble/imports/ui/components/AppRouter.jsx
@@ -2,15 +2,27 @@ import React, { Component } from 'react';
 import Home from '../components/Home/Home.jsx';
 import Account from '../components/Account/Account.jsx';
 import About from '../components/About/About.jsx';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import '../stylesheets/Heading.css';
 
 class AppRouter extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Route path="/" exact component={Home} />
-        <Route path="/about/" component={About} />
-        <Route path="/account/" component={Account} />
+        <div>
+          <img src='/Background/avatar.png' className='headImg'></img>
+          <h1 className='headId'>SoundBubble</h1>
+          <ul className='navContainer'>
+              <li><Link to={'/'} className='navLink'>Home</Link></li>
+              <li><Link to={'/Account'} className='navLink'>Account</Link></li>
+              <li><Link to={'/About'} className='navLink'>About</Link></li>
+          </ul>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about/" component={About} />
+            <Route path="/account/" component={Account} />
+          </Switch>
+      </div>
       </BrowserRouter>
     );
   }
