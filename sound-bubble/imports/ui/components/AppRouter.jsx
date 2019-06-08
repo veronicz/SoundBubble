1a30a02fbb2187b2e0118d1bcfc038ac4092e820
Talos <TalosOops@gmail.com>
2019-06-08 01:25:34 -0700

    Nav bar styling added

--- sound-bubble/imports/ui/components/AppRouter.jsx
+++ sound-bubble/imports/ui/components/AppRouter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Home from '../components/Home/Home.jsx';
 import Account from '../components/Account/Account.jsx';
 import About from '../components/About/About.jsx';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
 import '../stylesheets/Heading.css';
 
 class AppRouter extends Component {
@@ -12,11 +12,11 @@ class AppRouter extends Component {
         <div>
           <img src='/Background/avatar.png' className='headImg'></img>
           <h1 className='headId'>SoundBubble</h1>
-          <ul className='navContainer'>
-              <li><Link to={'/'} className='navLink'>Home</Link></li>
-              <li><Link to={'/Account'} className='navLink'>Account</Link></li>
-              <li><Link to={'/About'} className='navLink'>About</Link></li>
-          </ul>
+          <div className='navContainer'>
+              <NavLink exact to={'/'} className='navLink' activeClassName='activeLink'>Home</NavLink>
+              <NavLink to={'/Account'} className='navLink' activeClassName='activeLink'>Account</NavLink>
+              <NavLink to={'/About'} id='rest' className='navLink' activeClassName='activeLink'>About</NavLink>
+          </div>
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/about/" component={About} />
