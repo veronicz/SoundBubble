c3c0614192b46da0cf4f8f79cb16b1eeae3d0706
sz <v.sherryzzz@gmail.com>
2019-06-21 01:09:55 -0700

    Log in with Spotify
    - Added accounts-ui and blaze-react-component package to display the login button
    - Added xinranxiao:spotify-web-api and xinranxiao:accounts-spotify packages for a meteor wrapper to the Spotify API

--- sound-bubble/imports/ui/components/AppRouter.jsx
+++ sound-bubble/imports/ui/components/AppRouter.jsx
@@ -4,25 +4,47 @@ import Account from '../components/Account/Account.jsx';
 import About from '../components/About/About.jsx';
 import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
 import '../stylesheets/Heading.css';
+import Blaze from 'meteor/gadicc:blaze-react-component';
 
 class AppRouter extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          <img src='/Background/avatar.png' className='headImg'></img>
-          <h1 className='headId'>SoundBubble</h1>
-          <div className='navContainer'>
-              <NavLink exact to={'/'} className='navLink' activeClassName='activeLink'>Home</NavLink>
-              <NavLink to={'/Account'} className='navLink' activeClassName='activeLink'>Account</NavLink>
-              <NavLink to={'/About'} id='rest' className='navLink' activeClassName='activeLink'>About</NavLink>
+          <img src="/Background/avatar.png" className="headImg" />
+          <h1 className="headId">SoundBubble</h1>
+          <Blaze template="loginButtons" />
+          <div className="navContainer">
+            <NavLink
+              exact
+              to={'/'}
+              className="navLink"
+              activeClassName="activeLink"
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to={'/Account'}
+              className="navLink"
+              activeClassName="activeLink"
+            >
+              Account
+            </NavLink>
+            <NavLink
+              to={'/About'}
+              id="rest"
+              className="navLink"
+              activeClassName="activeLink"
+            >
+              About
+            </NavLink>
           </div>
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/about/" component={About} />
             <Route path="/account/" component={Account} />
           </Switch>
-      </div>
+        </div>
       </BrowserRouter>
     );
   }
