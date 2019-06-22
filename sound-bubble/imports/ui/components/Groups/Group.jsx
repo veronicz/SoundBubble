d7f14459daadf9db0c2f5398d34215b941fe7d6f
Martin Palanca <36322050+martipal@users.noreply.github.com>
2019-06-22 10:05:11 -0700

    Update Group.jsx

--- sound-bubble/imports/ui/components/Groups/Group.jsx
+++ sound-bubble/imports/ui/components/Groups/Group.jsx
@@ -6,8 +6,8 @@ export default class Group extends Component {
     return (<div>
         <div className="group_header">
         <h1 className="groupName"> Group 1 </h1> 
-        <div class="glyphicon glyphicon-remove-sign white"></div>
-        <div class="glyphicon glyphicon-plus-sign white"></div>
+        <div className="glyphicon glyphicon-remove-sign white"></div>
+        <div className="glyphicon glyphicon-plus-sign white"></div>
         </div>
 
 
