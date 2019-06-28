fa480675ed1c145e0c6ae54ddf4550cf0cac76f8
sz <v.sherryzzz@gmail.com>
2019-06-28 01:52:24 -0700

    Link Spotify Login to custom button
    - Linked Spotify login functionality to custom button instead of blaze template button
    - Also removed gadicc:blaze-react-component package, as it is no longer needed

--- sound-bubble/imports/ui/components/AppRouter.jsx
+++ sound-bubble/imports/ui/components/AppRouter.jsx
@@ -6,7 +6,7 @@ import Groups from '../components/Groups/Groups.jsx';
 import LogInPage from '../components/LogInPage.jsx';
 import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
 import '../stylesheets/Heading.css';
-import Blaze from 'meteor/gadicc:blaze-react-component';
+import AccountsUIWrapper from './AccountsUIWrapper.jsx';
 
 class AppRouter extends Component {
   constructor() {
@@ -16,64 +16,79 @@ class AppRouter extends Component {
 
   logIn() {
     this.setState({ loggedIn: true });
+    var options = {
+      showDialog: true, // Whether or not to force the user to approve the app again if they’ve already done so.
+      requestPermissions: [
+        'user-library-read',
+        'user-follow-read',
+        'playlist-read-private',
+        'user-read-recently-played'
+      ] // Spotify access scopes.
+    };
+
+    Meteor.loginWithSpotify(options, function(err) {
+      console.log(err || 'No error');
+    });
   }
 
   render() {
     if (this.state.loggedIn === true) {
       return (
         <BrowserRouter>
-        <div>
-          <div className = "headerContainer">
-            <div className="page_title_container">
-              <h1 className="header_page_title">SoundBubble</h1>
-            </div>
-            <Blaze template="loginButtons" />
-            <div className="navContainer">
-              <NavLink
-                exact
-                to={'/'}
-                className="navLink"
-                activeClassName="activeLink"
-              >
-                <span className="glyphicon glyphicon-home" area-hidden="true"></span>Home
-                
-            </NavLink>
-              <NavLink
-                to={'/Account'}
-                className="navLink"
-                activeClassName="activeLink"
-              >
-                My Profile
-            </NavLink>
-            <NavLink
-                to={'/Groups'}
-                className="navLink"
-                activeClassName="activeLink"
-              >
-                My Groups
-            </NavLink>
-              <NavLink
-                to={'/About'}
-                id="rest"
-                className="navLink"
-                activeClassName="activeLink"
-              >
-                About
-            </NavLink>
-            </div>
+          <div>
+            <div className="headerContainer">
+              <div className="page_title_container">
+                <h1 className="header_page_title">SoundBubble</h1>
+              </div>
+              <AccountsUIWrapper />
+              <div className="navContainer">
+                <NavLink
+                  exact
+                  to={'/'}
+                  className="navLink"
+                  activeClassName="activeLink"
+                >
+                  <span
+                    className="glyphicon glyphicon-home"
+                    area-hidden="true"
+                  />
+                  Home
+                </NavLink>
+                <NavLink
+                  to={'/Account'}
+                  className="navLink"
+                  activeClassName="activeLink"
+                >
+                  My Profile
+                </NavLink>
+                <NavLink
+                  to={'/Groups'}
+                  className="navLink"
+                  activeClassName="activeLink"
+                >
+                  My Groups
+                </NavLink>
+                <NavLink
+                  to={'/About'}
+                  id="rest"
+                  className="navLink"
+                  activeClassName="activeLink"
+                >
+                  About
+                </NavLink>
+              </div>
             </div>
             <Switch>
               <Route path="/" exact component={Home} />
               <Route path="/about/" component={About} />
               <Route path="/account/" component={Account} />
-              <Route path="/groups/" component={Groups}/>
+              <Route path="/groups/" component={Groups} />
             </Switch>
           </div>
-
         </BrowserRouter>
       );
     } else {
-      return (<LogInPage logIn={this.logIn.bind(this)} />)
+      return <LogInPage logIn={this.logIn.bind(this)} />;
     }
   }
 }
--- sound-bubble/imports/ui/components/LogInPage.jsx
+++ sound-bubble/imports/ui/components/LogInPage.jsx
@@ -1,27 +1,23 @@
 import React, { Component } from 'react';
 import '../stylesheets/LogIn.css';
-import Blaze from 'meteor/gadicc:blaze-react-component';
 
 export default class LogInPage extends Component {
-  
-  logIn(){
-      this.setState({loggedIn: true});
-  }
-
   render() {
-      let logIn = this.props.logIn;
-    return (<div className="welcome_container">
-    <h1 className="page_title">
-    SoundBubble
-    </h1>
-    <p className="welcome_description">
-     SoundBubble integrates users' Spotify music listening history via Spotify's public API. To begin using this application and sharing music, you must be subscribed to Spotify and enable API access to your user account. Terms and conditions are outlined when logging in. To begin, log in with Spotify below.
-    </p>
-    <button className="login"  onClick={() => logIn()}>
-    Login with Spotify
+    let logIn = this.props.logIn;
+    return (
+      <div className="welcome_container">
+        <h1 className="page_title">SoundBubble</h1>
+        <p className="welcome_description">
+          SoundBubble integrates users' Spotify music listening history via
+          Spotify's public API. To begin using this application and sharing
+          music, you must be subscribed to Spotify and enable API access to your
+          user account. Terms and conditions are outlined when logging in. To
+          begin, log in with Spotify below.
+        </p>
+        <button className="login" onClick={() => logIn()}>
+          Login with Spotify
         </button>
-  
-    </div>);
+      </div>
+    );
   }
 }
-
