d8cd01f3a3482eeca382921ba177eeb4a695143a
Martin Palanca <36322050+martipal@users.noreply.github.com>
2019-07-03 12:43:07 -0700

    Revert "Finished Groups Page Styling"
    This reverts commit 7be46326d6b72312580159fa3f80caf008bbe46f.

--- sound-bubble/imports/ui/components/Groups/Groups.jsx
+++ sound-bubble/imports/ui/components/Groups/Groups.jsx
@@ -3,28 +3,10 @@ import '../../stylesheets/Groups.css';
 import Group from './Group.jsx';
 
 export default class Groups extends Component {
-
-  createGroup(){
-    // TODO: Create new group in db
-  }
-
-  getGroupsForCurrentUser(){
-    //TODO: get groups for current user
-  }
-
-  createGroupComponents(){
-    //TODO: create a div containing all Groups that the user belongs to. Iterate through their groups and create a <Group> component for each one with props groupName, groupID, userIds. 
-  }
-
   render() {
     return (<div className="groups_container">
-      <div className="heading_container">
-        <h1 className="group_heading"> Groups </h1><div className="group_options"><div className="option_container" onClick={() => this.createGroup()}><div className="glyphicon glyphicon-plus white"><span className="tooltiptext">Create New Group</span></div></div></div>
-      </div>
-      
-      <Group groupName="" groupId="" usersIds=""/>
-
-
+        <h1 className="group_heading"> Groups </h1>
+        <Group/>
     </div>);
 
   }
