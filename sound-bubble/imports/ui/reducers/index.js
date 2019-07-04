a235c4cd04ef8db98a2e49c590a90dfb697f6dac
sz <v.sherryzzz@gmail.com>
2019-07-04 16:05:56 -0700

    Remove CurrentUserReducer from combineReducers

--- sound-bubble/imports/ui/reducers/index.js
+++ sound-bubble/imports/ui/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import SongLogReducer from './SongLogReducer';
 import UserProfileReducer from './UserProfileReducer';
-import CurrentUserReducer from './CurrentUserReducer';
 
 export default combineReducers({
   tracks: SongLogReducer,
-  profiles: UserProfileReducer,
-  user: CurrentUserReducer
+  profiles: UserProfileReducer
 });
