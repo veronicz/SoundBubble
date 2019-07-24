a78abf64d1ce7434a54878a101110609a66ac9d4
sz <v.sherryzzz@gmail.com>
2019-07-23 19:39:32 -0700

    Clears current group when user logs out

--- sound-bubble/imports/ui/components/Account/User.jsx
+++ sound-bubble/imports/ui/components/Account/User.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import compose from 'recompose/compose';
+import { connect } from 'react-redux';
 import { withTracker } from 'meteor/react-meteor-data';
-import AccountsUIWrapper from './AccountsUIWrapper.jsx';
 import Groups from '../../../api/groups';
 import { Meteor } from 'meteor/meteor';
+import { removeCurrentGroup } from '../../actions/groupActions';
 
 class User extends Component {
   formatUserInfo(title, detail) {
@@ -12,6 +14,11 @@ class User extends Component {
         <dd>{detail}</dd>
       </dl>
     );
+  }
+
+  logout() {
+    Meteor.logout();
+    this.props.removeCurrentGroup();
   }
 
   render() {
@@ -33,13 +40,24 @@ class User extends Component {
             {this.formatUserInfo('Groups', myGroupsCount)}
             {this.formatUserInfo('Email', user.email)}
           </div>
-          <a className="profileLink" href={spotifyUrl}>Go To Your Spotify</a>
+          <a className="profileLink" href={spotifyUrl}>
+            Go To Your Spotify
+          </a>
           <br />
-          <a className="profileLink" href="https://www.spotify.com/account/apps/?_ga=2.100345246.1881641136.1561931040-1289360233.1558571744">
+          <a
+            className="profileLink"
+            href="https://www.spotify.com/account/apps/?_ga=2.100345246.1881641136.1561931040-1289360233.1558571744"
+          >
             Manage Spotify App Access
           </a>
           <div className="logout">
-            <AccountsUIWrapper />
+            <button
+              type="button"
+              class="btn btn-secondary"
+              onClick={() => this.logout()}
+            >
+              Log out
+            </button>
           </div>
         </div>
       </div>
@@ -47,9 +65,15 @@ class User extends Component {
   }
 }
 
-export default withTracker(props => {
-  const myGroupsReady = Meteor.subscribe('myGroups').ready();
-  return {
-    myGroupsCount: myGroupsReady ? Groups.find().fetch().length : 0
-  };
-})(User);
+export default compose(
+  withTracker(props => {
+    const myGroupsReady = Meteor.subscribe('myGroups').ready();
+    return {
+      myGroupsCount: myGroupsReady ? Groups.find().fetch().length : 0
+    };
+  }),
+  connect(
+    null,
+    { removeCurrentGroup }
+  )
+)(User);
