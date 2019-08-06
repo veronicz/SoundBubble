55e9699d62a8a6f2cc3e33484145dd43b9a34334
sz <v.sherryzzz@gmail.com>
2019-08-05 21:51:50 -0700

    Resolve console error
    - changed background-color to backgroundColor

--- sound-bubble/imports/ui/components/Account/HideSongButton.jsx
+++ sound-bubble/imports/ui/components/Account/HideSongButton.jsx
@@ -7,28 +7,28 @@ class HideSongButton extends Component {
     const { hideMySong, song, show } = this.props;
 
     let button;
-    if (show){
-      button = (<button
-        className="hide_button"
-        onClick={() => hideMySong(song._id, show ? 'hide' : 'show')}
-      >
-        Hide
-      </button>);
+    if (show) {
+      button = (
+        <button
+          className="hide_button"
+          onClick={() => hideMySong(song._id, show ? 'hide' : 'show')}
+        >
+          Hide
+        </button>
+      );
     } else {
-      button = (<button
-        className="hide_button"
-        style={{'background-color':'black', 'color':'white'}}
-        onClick={() => hideMySong(song._id, show ? 'hide' : 'show')}
-      >
-        Show
-      </button>);
+      button = (
+        <button
+          className="hide_button"
+          style={{ backgroundColor: 'black', color: 'white' }}
+          onClick={() => hideMySong(song._id, show ? 'hide' : 'show')}
+        >
+          Show
+        </button>
+      );
     }
 
-    return (
-      <div className="hide_song">
-        {button}
-      </div>
-    );
+    return <div className="hide_song">{button}</div>;
   }
 }
 
